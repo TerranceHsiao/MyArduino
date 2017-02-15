d8a7e7d858259847fb076943e99d88465bdfb64c
terrance_hsiao <agptau684@gmail.com>
2017-02-16 01:11:09 +0800

    test for commit

---
+++ libraries/kernel/inc/type.h
@@ -0,0 +1,14 @@
+
+typedef char S08;
+typedef short int S16;
+typedef long  int S32;
+typedef long long int S64;
+
+typedef unsigned char U08;
+typedef unsigned short int U16;
+typedef unsigned long  int U32;
+typedef unsigned long long int U64;
+
+typedef float F32;
+typedef double D64;
+
